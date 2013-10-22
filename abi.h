0b7297d6bbd3fe5322bfdd61e47197d3987a7670
Ali Polatel <alip@exherbo.org>
2013-10-23 00:28:41 +0300

    update doxygen configuration

--- abi.h
+++ abi.h
@@ -43,29 +43,46 @@
  * @{
  **/
 
-#if PINK_ARCH_X86_64
-# define PINK_ABIS_SUPPORTED 3
-# define PINK_ABI_X86_64 0
-# define PINK_ABI_I386 1
-# define PINK_ABI_X32 2
+#ifndef DOXYGEN
+
+# if PINK_ARCH_X86_64
+#  define PINK_ABIS_SUPPORTED 3
+#  define PINK_ABI_X86_64 0
+#  define PINK_ABI_I386 1
+#  define PINK_ABI_X32 2
 #endif
 
-#if PINK_ARCH_X32
-# define PINK_ABIS_SUPPORTED 2
-# define PINK_ABI_X32 0
-# define PINK_ABI_I386 1
-#endif
+# if PINK_ARCH_X32
+#  define PINK_ABIS_SUPPORTED 2
+#  define PINK_ABI_X32 0
+#  define PINK_ABI_I386 1
+# endif
 
-#if PINK_ARCH_POWERPC64
-# define PINK_ABIS_SUPPORTED 2
-# define PINK_ABI_PPC64 0
-# define PINK_ABI_PPC32 1
-#endif
+# if PINK_ARCH_POWERPC64
+#  define PINK_ABIS_SUPPORTED 2
+#  define PINK_ABI_PPC64 0
+#  define PINK_ABI_PPC32 1
+# endif
 
-#ifndef PINK_ABIS_SUPPORTED
-# define PINK_ABIS_SUPPORTED 1
+# ifndef PINK_ABIS_SUPPORTED
+#  define PINK_ABIS_SUPPORTED 1
+# endif
+# define PINK_ABI_DEFAULT 0
+
+#else
+/**
+ * Default ABI of the host architecture
+ *
+ * Consult the definitions under <pinktrace/abi.h> for more information.
+ **/
+# define PINK_ABI_DEFAULT	0
+/**
+ * Total number of supported ABIs
+ *
+ * Consult the definitions under <pinktrace/abi.h> for more information.
+ */
+# define PINK_ABIS_SUPPORTED	-1
 #endif
-#define PINK_ABI_DEFAULT 0
 
 /**
  * Return the word size of the system call ABI
