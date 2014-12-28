d629a152dcccbc6be4102e7fddf66e1f470e4bc3
Ali Polatel <alip@exherbo.org>
2014-12-28 13:06:35 +0200

    add prep-env.sh scripts

---
+++ misc/prep-env.sh
@@ -0,0 +1,16 @@
+#!/bin/sh -x
+
+CFLAGS="-D__ALIP_WAS_HERE"
+CFLAGS="${CFLAGS} -pedantic -W -Wall -Wextra -Wshadow -Wno-unused-parameter"
+CFLAGS="${CFLAGS} -O0 -g -ggdb3"
+CFLAGS="${CFLAGS} -Wall"
+CFLAGS="${CFLAGS} -Werror=implicit-function-declaration"
+CFLAGS="${CFLAGS} -Werror=implicit-int"
+CFLAGS="${CFLAGS} -Werror=pointer-sign"
+CFLAGS="${CFLAGS} -Werror=pointer-arith"
+CFLAGS="${CFLAGS} -D__PINK_IS_BEHIND_THE_WALL"
+export CFLAGS
+
+MALLOC_CHECK_=3
+MALLOC_PERTURB_=$(($RANDOM % 255 + 1))
+export MALLOC_CHECK_ MALLOC_PERTURB_
