alip/pinktrace

commit 2ca0b965e4508285e64e32df31f8574dc8f544af

update system call tables

pinktrace/linux/arm/syscallent.h
@@ -376,19 +376,5 @@
 	"setns", /* 375 */
 	"process_vm_readv", /* 376 */
 	"process_vm_writev", /* 377 */
-#define ARM_LAST_ORDINARY_SYSCALL 377
-	/* __ARM_NR_cmpxchg (0x000ffff0).
-	 * Remapped by shuffle_scno() to be directly after ordinary syscalls
-	 * in this table.
-	 */
-	"cmpxchg",
-	/* ARM specific syscalls. Encoded with scno 0x000f00xx.
-	 * Remapped by shuffle_scno() to be directly after __ARM_NR_cmpxchg.
-	 */
-	NULL, /* 0 */
-	"breakpoint", /* 1 */
-	"cacheflush", /* 2 */
-	"usr26", /* 3 */
-	"usr32", /* 4 */
-	"set_tls", /* 5 */
-#define ARM_LAST_SPECIAL_SYSCALL 5
+	"kcmp", /* 378 */
+	"finit_module", /* 379 */

pinktrace/linux/i386/syscallent.h
@@ -347,3 +347,5 @@
 	"setns", /* 346 */
 	"process_vm_readv", /* 347 */
 	"process_vm_writev", /* 348 */
+	"kcmp", /* 349 */
+	"finit_module", /* 350 */

pinktrace/linux/ia64/syscallent.h
@@ -95,12 +95,12 @@
 	"syslog", /* 1117 */
 	"setitimer", /* 1118 */
 	"getitimer", /* 1119 */
-	"old_stat", /* 1120 */
-	"old_lstat", /* 1121 */
-	"old_fstat", /* 1122 */
+	"stat", /* 1120 */
+	"lstat", /* 1121 */
+	"fstat", /* 1122 */
 	"vhangup", /* 1123 */
 	"lchown", /* 1124 */
-	"remap_file_pages", /* 1125 */
+	"vm86", /* 1125 */
 	"wait4", /* 1126 */
 	"sysinfo", /* 1127 */
 	"clone", /* 1128 */
@@ -123,8 +123,8 @@
 	"flock", /* 1145 */
 	"readv", /* 1146 */
 	"writev", /* 1147 */
-	"pread64", /* 1148 */
-	"pwrite64", /* 1149 */
+	"pread", /* 1148 */
+	"pwrite", /* 1149 */
 	"_sysctl", /* 1150 */
 	"mmap", /* 1151 */
 	"munmap", /* 1152 */
@@ -206,8 +206,8 @@
 	"fremovexattr", /* 1228 */
 	"tkill", /* 1229 */
 	"futex", /* 1230 */
-	"sched_setaffinity", /* 1231 */
-	"sched_getaffinity", /* 1232 */
+	"sched_setaffinity",/* 1231 */
+	"sched_getaffinity",/* 1232 */
 	"set_tid_address", /* 1233 */
 	"fadvise64", /* 1234 */
 	"tgkill", /* 1235 */
@@ -272,35 +272,33 @@
 	"pselect6", /* 1294 */
 	"ppoll", /* 1295 */
 	"unshare", /* 1296 */
-	"splice", /* 1297 */
-	"set_robust_list", /* 1298 */
-	"get_robust_list", /* 1299 */
+	"set_robust_list", /* 1297 */
+	"get_robust_list", /* 1298 */
+	"splice", /* 1299 */
 	"sync_file_range", /* 1300 */
 	"tee", /* 1301 */
 	"vmsplice", /* 1302 */
-	"fallocate", /* 1303 */
+	NULL, /* 1303 */
 	"getcpu", /* 1304 */
 	"epoll_pwait", /* 1305 */
-	"utimensat", /* 1306 */
+	NULL, /* 1306 */
 	"signalfd", /* 1307 */
 	"timerfd", /* 1308 */
 	"eventfd", /* 1309 */
-	"timerfd_create", /* 1310 */
-	"timerfd_settime", /* 1311 */
-	"timerfd_gettime", /* 1312 */
-	"preadv", /* 1313 */
-	"pwritev", /* 1314 */
-	"rt_tgsigqueueinfo" /* 1315 */
-	"recvmmsg", /* 1316 */
-	"fanotify_init", /* 1317 */
-	"fanotify_mark", /* 1318 */
-	"prlimit64", /* 1319 */
-	"name_to_handle_at", /* 1320 */
-	"open_by_handle_at", /* 1321 */
-	"clock_adjtime", /* 1322 */
-	"syncfs", /* 1323 */
-	"setns", /* 1324 */
-	"sendmmsg", /* 1325 */
-	"process_vm_readv", /* 1326 */
-	"process_vm_writev", /* 1327 */
-	"accept4", /* 1328 */
+	"preadv", /* 1319 */
+	"pwritev", /* 1320 */
+	"rt_tgsigqueueinfo", /* 1321 */
+	"recvmmsg", /* 1322 */
+	"fanotify_init", /* 1323 */
+	"fanotify_mark", /* 1324 */
+	"prlimit64", /* 1325 */
+	"name_to_handle_at", /* 1326 */
+	"open_by_handle_at", /* 1327 */
+	"clock_adjtime", /* 1328 */
+	"syncfs", /* 1329 */
+	"setns", /* 1330 */
+	"sendmmsg", /* 1331 */
+	"process_vm_readv", /* 1332 */
+	"process_vm_writev", /* 1333 */
+	"accept4", /* 1334 */
+	"finit_module", /* 1335 */

pinktrace/linux/powerpc/syscallent.h
@@ -351,3 +351,5 @@
 	"setns", /* 350 */
 	"process_vm_readv", /* 351 */
 	"process_vm_writev", /* 352 */
+	"kcmp", /* 353 */
+	"finit_module", /* 354 */

pinktrace/linux/x32/syscallent.h
@@ -539,3 +539,5 @@
 	"sendmmsg", /* 538 */
 	"process_vm_readv", /* 539 */
 	"process_vm_writev", /* 540 */
+	"kcmp", /* 312 */
+	"finit_module", /* 313 */

pinktrace/linux/x86_64/syscallent.h
@@ -310,3 +310,5 @@
 	"getcpu", /* 309 */
 	"process_vm_readv", /* 310 */
 	"process_vm_writev", /* 311 */
+	"kcmp", /* 312 */
+	"finit_module", /* 313 */

commit fe89eb08d1e70e31d6a9a71fc447d6fa1626b9cd

Update syscall table and fix shuffle_scno for ARM
The shuffle_scno function was broken in commit 2ca0b96 due to the
removal of the ARM_LAST_ORDINARY_SYSCALL definition.

This updates the syscall table and makes the shuffle_scno function
more readable analogue to the way it was changed in strace

Change-Id: Id2c9bd196d2e1950f87e548bef5c28abcc305aad
Reviewed-on: https://galileo.mailstation.de/gerrit/3392
Reviewed-by: Jenkins <philantrop@gmail.com>
Reviewed-by: Kylie McClain <somasis@exherbo.org>

pinktrace/linux/arm/syscallent.h
@@ -378,3 +378,32 @@
 	"process_vm_writev", /* 377 */
 	"kcmp", /* 378 */
 	"finit_module", /* 379 */
+	"sched_setattr", /* 380 */
+	"sched_getattr", /* 381 */
+	"renameat2", /* 382 */
+	"seccomp", /* 383 */
+	"getrandom", /* 384 */
+	"memfd_create", /* 385 */
+	"bpf", /* 386 */
+	"execveat", /* 387 */
+
+#define ARM_FIRST_SHUFFLED_SYSCALL 388
+
+	/* __ARM_NR_cmpxchg (0x000ffff0).
+	 * Remapped by shuffle_scno() to be directly after ordinary syscalls
+	 * in this table.
+	 */
+	"cmpxchg", /* ARM_FIRST_SHUFFLED_SYSCALL */
+
+	/* ARM specific syscalls. Encoded with scno 0x000f00xx.
+	 * Remapped by shuffle_scno() to be directly after __ARM_NR_cmpxchg.
+	 */
+        NULL, /* 0 */
+	"breakpoint", /* 1 */
+	"cacheflush", /* 2 */
+	"usr26", /* 3 */
+	"usr32", /* 4 */
+	"set_tls" /* 5 */
+
+#define ARM_LAST_SPECIAL_SYSCALL 5
+

pinktrace/name.c
@@ -247,39 +247,41 @@ static const struct xlat addrfams[] = {
 	{ 0,		NULL		},
 };
 
-/* Shuffle syscall numbers so that we don't have huge gaps in syscall table.
- * The shuffling should be reversible: shuffle_scno(shuffle_scno(n)) == n.
+/*
+ * Shuffle syscall numbers so that we don't have huge gaps in syscall table.
+ * The shuffling should be an involution: shuffle_scno(shuffle_scno(n)) == n.
  */
-#if PINK_ARCH_ARM /* So far only ARM needs this */
-static long shuffle_scno(unsigned long scno)
+#if defined(ARM) || defined(AARCH64) /* So far only 32-bit ARM needs this */
+static long
+shuffle_scno(unsigned long scno)
 {
-	if (scno <= ARM_LAST_ORDINARY_SYSCALL)
+	if (scno < ARM_FIRST_SHUFFLED_SYSCALL)
 		return scno;
 
 	/* __ARM_NR_cmpxchg? Swap with LAST_ORDINARY+1 */
-	if (scno == 0x000ffff0)
-		return ARM_LAST_ORDINARY_SYSCALL+1;
-	if (scno == ARM_LAST_ORDINARY_SYSCALL+1)
+	if (scno == ARM_FIRST_SHUFFLED_SYSCALL)
 		return 0x000ffff0;
+	if (scno == 0x000ffff0)
+		return ARM_FIRST_SHUFFLED_SYSCALL;
 
-	/* Is it ARM specific syscall?
-	 * Swap with [LAST_ORDINARY+2, LAST_ORDINARY+2 + LAST_SPECIAL] range.
+#define ARM_SECOND_SHUFFLED_SYSCALL (ARM_FIRST_SHUFFLED_SYSCALL + 1)
+	/*
+	 * Is it ARM specific syscall?
+	 * Swap [0x000f0000, 0x000f0000 + LAST_SPECIAL] range
+	 * with [SECOND_SHUFFLED, SECOND_SHUFFLED + LAST_SPECIAL] range.
 	 */
-	if (scno >= 0x000f0000
-	 && scno <= 0x000f0000 + ARM_LAST_SPECIAL_SYSCALL
-	) {
-		return scno - 0x000f0000 + (ARM_LAST_ORDINARY_SYSCALL+2);
+	if (scno >= 0x000f0000 &&
+	    scno <= 0x000f0000 + ARM_LAST_SPECIAL_SYSCALL) {
+		return scno - 0x000f0000 + ARM_SECOND_SHUFFLED_SYSCALL;
 	}
-	if (/* scno >= ARM_LAST_ORDINARY_SYSCALL+2 - always true */ 1
-	 && scno <= (ARM_LAST_ORDINARY_SYSCALL+2) + ARM_LAST_SPECIAL_SYSCALL
-	) {
-		return scno + 0x000f0000 - (ARM_LAST_ORDINARY_SYSCALL+2);
+	if (scno <= ARM_SECOND_SHUFFLED_SYSCALL + ARM_LAST_SPECIAL_SYSCALL) {
+		return scno + 0x000f0000 - ARM_SECOND_SHUFFLED_SYSCALL;
 	}
 
 	return scno;
 }
 #else
-# define shuffle_scno(scno) (long)(scno)
+# define shuffle_scno(scno) ((long)(scno))
 #endif
 
 PINK_GCC_ATTR((pure))