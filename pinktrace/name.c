3427ebd1b50c91384c10c69d170d06f06bbffea2
Ali Polatel <alip@exherbo.org>
2021-03-14 13:57:23 +0100

    rearrange xlat to close 4 bytes hole
    Signed-off-by: Ali Polatel <alip@exherbo.org>

--- pinktrace/name.c
+++ pinktrace/name.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010, 2011, 2012, 2013 Ali Polatel <alip@exherbo.org>
+ * Copyright (c) 2010, 2011, 2012, 2013, 2021 Ali Polatel <alip@exherbo.org>
  * Based in part upon strace which is:
  *   Copyright (c) 1991, 1992 Paul Kranenburg <pk@cs.few.eur.nl>
  *   Copyright (c) 1993 Branko Lankester <branko@hacktic.nl>
@@ -113,138 +113,138 @@ static const char *const *signalent_vec[PINK_ABIS_SUPPORTED] = {
 };
 
 struct xlat {
-	int val;
 	const char *str;
+	int val;
 };
 
 static const struct xlat events[] = {
-	{PINK_EVENT_FORK,	"FORK"},
-	{PINK_EVENT_VFORK,	"VFORK"},
-	{PINK_EVENT_CLONE,	"CLONE"},
-	{PINK_EVENT_EXEC,	"EXEC"},
-	{PINK_EVENT_VFORK_DONE,	"VFORK_DONE"},
-	{PINK_EVENT_EXIT,	"EXIT"},
-	{PINK_EVENT_SECCOMP,	"SECCOMP"},
-	{PINK_EVENT_STOP,	"STOP"},
-	{0,			NULL},
+	{"FORK",	PINK_EVENT_FORK},
+	{"VFORK",	PINK_EVENT_VFORK},
+	{"CLONE",	PINK_EVENT_CLONE},
+	{"EXEC",	PINK_EVENT_EXEC},
+	{"VFORK_DONE",	PINK_EVENT_VFORK_DONE},
+	{"EXIT",	PINK_EVENT_EXIT},
+	{"SECCOMP",	PINK_EVENT_SECCOMP},
+	{"STOP",	PINK_EVENT_STOP},
+	{NULL,		0},
 };
 
 static const struct xlat socket_subcalls[] = {
-	{PINK_SOCKET_SUBCALL_BIND,		"bind"},
-	{PINK_SOCKET_SUBCALL_CONNECT,		"connect"},
-	{PINK_SOCKET_SUBCALL_LISTEN,		"listen"},
-	{PINK_SOCKET_SUBCALL_ACCEPT,		"accept"},
-	{PINK_SOCKET_SUBCALL_GETSOCKNAME,	"getsockname"},
-	{PINK_SOCKET_SUBCALL_GETPEERNAME,	"getpeername"},
-	{PINK_SOCKET_SUBCALL_SOCKETPAIR,	"socketpair"},
-	{PINK_SOCKET_SUBCALL_SEND,		"send"},
-	{PINK_SOCKET_SUBCALL_RECV,		"recv"},
-	{PINK_SOCKET_SUBCALL_SENDTO,		"sendto"},
-	{PINK_SOCKET_SUBCALL_RECVFROM,		"recvfrom"},
-	{PINK_SOCKET_SUBCALL_SHUTDOWN,		"shutdown"},
-	{PINK_SOCKET_SUBCALL_SETSOCKOPT,	"setsockopt"},
-	{PINK_SOCKET_SUBCALL_GETSOCKOPT,	"getsockopt"},
-	{PINK_SOCKET_SUBCALL_SENDMSG,		"sendmsg"},
-	{PINK_SOCKET_SUBCALL_RECVMSG,		"recvmsg"},
-	{PINK_SOCKET_SUBCALL_ACCEPT4,		"accept4"},
-	{0,					NULL},
+	{"bind",		PINK_SOCKET_SUBCALL_BIND},
+	{"connect",		PINK_SOCKET_SUBCALL_CONNECT},
+	{"listen",		PINK_SOCKET_SUBCALL_LISTEN},
+	{"accept",		PINK_SOCKET_SUBCALL_ACCEPT},
+	{"getsockname",		PINK_SOCKET_SUBCALL_GETSOCKNAME},
+	{"getpeername",		PINK_SOCKET_SUBCALL_GETPEERNAME},
+	{"socketpair",		PINK_SOCKET_SUBCALL_SOCKETPAIR},
+	{"send",		PINK_SOCKET_SUBCALL_SEND},
+	{"recv",		PINK_SOCKET_SUBCALL_RECV},
+	{"sendto",		PINK_SOCKET_SUBCALL_SENDTO},
+	{"recvfrom",		PINK_SOCKET_SUBCALL_RECVFROM},
+	{"shutdown",		PINK_SOCKET_SUBCALL_SHUTDOWN},
+	{"setsockopt",		PINK_SOCKET_SUBCALL_SETSOCKOPT},
+	{"getsockopt",		PINK_SOCKET_SUBCALL_GETSOCKOPT},
+	{"sendmsg",		PINK_SOCKET_SUBCALL_SENDMSG},
+	{"recvmsg",		PINK_SOCKET_SUBCALL_RECVMSG},
+	{"accept4",		PINK_SOCKET_SUBCALL_ACCEPT4},
+	{NULL,			0},
 };
 
 static const struct xlat addrfams[] = {
 #ifdef AF_APPLETALK
-	{ AF_APPLETALK,	"AF_APPLETALK"	},
+	{ "AF_APPLETALK",	AF_APPLETALK},
 #endif
 #ifdef AF_ASH
-	{ AF_ASH,	"AF_ASH"	},
+	{ "AF_ASH",	AF_ASH},
 #endif
 #ifdef AF_ATMPVC
-	{ AF_ATMPVC,	"AF_ATMPVC"	},
+	{ "AF_ATMPVC",	AF_ATMPVC},
 #endif
 #ifdef AF_ATMSVC
-	{ AF_ATMSVC,	"AF_ATMSVC"	},
+	{ "AF_ATMSVC",	AF_ATMSVC},
 #endif
 #ifdef AF_AX25
-	{ AF_AX25,	"AF_AX25"	},
+	{ "AF_AX25",	AF_AX25},
 #endif
 #ifdef AF_BLUETOOTH
-	{ AF_BLUETOOTH,	"AF_BLUETOOTH"	},
+	{ "AF_BLUETOOTH",	AF_BLUETOOTH},
 #endif
 #ifdef AF_BRIDGE
-	{ AF_BRIDGE,	"AF_BRIDGE"	},
+	{ "AF_BRIDGE",	AF_BRIDGE},
 #endif
 #ifdef AF_DECnet
-	{ AF_DECnet,	"AF_DECnet"	},
+	{ "AF_DECnet",	AF_DECnet},
 #endif
 #ifdef AF_ECONET
-	{ AF_ECONET,	"AF_ECONET"	},
+	{ "AF_ECONET",	AF_ECONET},
 #endif
 #ifdef AF_FILE
-	{ AF_FILE,	"AF_FILE"	},
+	{ "AF_FILE",	AF_FILE},
 #endif
 #ifdef AF_IMPLINK
-	{ AF_IMPLINK,	"AF_IMPLINK"	},
+	{ "AF_IMPLINK",	AF_IMPLINK},
 #endif
 #ifdef AF_INET
-	{ AF_INET,	"AF_INET"	},
+	{ "AF_INET",	AF_INET},
 #endif
 #ifdef AF_INET6
-	{ AF_INET6,	"AF_INET6"	},
+	{ "AF_INET6",	AF_INET6},
 #endif
 #ifdef AF_IPX
-	{ AF_IPX,	"AF_IPX"	},
+	{ "AF_IPX",	AF_IPX},
 #endif
 #ifdef AF_IRDA
-	{ AF_IRDA,	"AF_IRDA"	},
+	{ "AF_IRDA",	AF_IRDA},
 #endif
 #ifdef AF_ISO
-	{ AF_ISO,	"AF_ISO"	},
+	{ "AF_ISO",	AF_ISO},
 #endif
 #ifdef AF_KEY
-	{ AF_KEY,	"AF_KEY"	},
+	{ "AF_KEY",	AF_KEY},
 #endif
 #ifdef AF_UNIX
-	{ AF_UNIX,	"AF_UNIX"	},
+	{ "AF_UNIX",	AF_UNIX},
 #endif
 #ifdef AF_LOCAL
-	{ AF_LOCAL,	"AF_LOCAL"	},
+	{ "AF_LOCAL",	AF_LOCAL},
 #endif
 #ifdef AF_NETBEUI
-	{ AF_NETBEUI,	"AF_NETBEUI"	},
+	{ "AF_NETBEUI",	AF_NETBEUI},
 #endif
 #ifdef AF_NETLINK
-	{ AF_NETLINK,	"AF_NETLINK"	},
+	{ "AF_NETLINK",	AF_NETLINK},
 #endif
 #ifdef AF_NETROM
-	{ AF_NETROM,	"AF_NETROM"	},
+	{ "AF_NETROM",	AF_NETROM},
 #endif
 #ifdef AF_PACKET
-	{ AF_PACKET,	"AF_PACKET"	},
+	{ "AF_PACKET",	AF_PACKET},
 #endif
 #ifdef AF_PPPOX
-	{ AF_PPPOX,	"AF_PPPOX"	},
+	{ "AF_PPPOX",	AF_PPPOX},
 #endif
 #ifdef AF_ROSE
-	{ AF_ROSE,	"AF_ROSE"	},
+	{ "AF_ROSE",	AF_ROSE},
 #endif
 #ifdef AF_ROUTE
-	{ AF_ROUTE,	"AF_ROUTE"	},
+	{ "AF_ROUTE",	AF_ROUTE},
 #endif
 #ifdef AF_SECURITY
-	{ AF_SECURITY,	"AF_SECURITY"	},
+	{ "AF_SECURITY",	AF_SECURITY},
 #endif
 #ifdef AF_SNA
-	{ AF_SNA,	"AF_SNA"	},
+	{ "AF_SNA",	AF_SNA},
 #endif
 #ifdef AF_UNSPEC
-	{ AF_UNSPEC,	"AF_UNSPEC"	},
+	{ "AF_UNSPEC",	AF_UNSPEC},
 #endif
 #ifdef AF_WANPIPE
-	{ AF_WANPIPE,	"AF_WANPIPE"	},
+	{ "AF_WANPIPE",	AF_WANPIPE},
 #endif
 #ifdef AF_X25
-	{ AF_X25,	"AF_X25"	},
+	{ "AF_X25",	AF_X25},
 #endif
-	{ 0,		NULL		},
+	{ NULL,		0},
 };
 
 /*
